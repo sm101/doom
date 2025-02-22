b8be0cd6e541d4e46961c81d3b61f09fe81ab37b
Stevan Markovic <smarkovic@stevan-mba.local>
2025-02-21 21:39:34 -0500

    Remove chatgpt check paragraph.
    The similar integration is now in chatgpt-shell-proofread-region.

--- config.el
+++ config.el
@@ -162,32 +162,6 @@
  :config
  (setq! gptel-api-key (getenv "OPENAI_API_KEY")))
 
-(defun chatgpt-shell-check-and-correct-paragraph ()
-  "Check and correct the current paragraph using ChatGPT."
-  (interactive)
-  (let* ((bounds (bounds-of-thing-at-point 'paragraph))
-         (start (car bounds))
-         (end (cdr bounds))
-         (original-text (buffer-substring-no-properties start end))
-         (point-offset (- end (point))) ; Save the offset of point from end
-         (checked-text (chatgpt-shell-check-paragraph original-text)))
-    (when checked-text
-      (save-excursion
-        (goto-char start)
-        (delete-region start end)
-        (insert "\n") ; Insert a newline here
-        (insert checked-text))
-      ;; Move the point to its new position relative to the end
-      (goto-char (- (cdr (bounds-of-thing-at-point 'paragraph)) point-offset)))))
-
-(defun chatgpt-shell-check-paragraph (text)
-  "Send TEXT to ChatGPT for spell and grammar checking."
-  (chatgpt-shell-post-chatgpt-messages :messages `(((role . "user") (content . ,(concat
-                     "Please correct the spelling and grammar of the following paragraph."
-                     "Maintain existing org-mode syntax expressions like =this= and *this*."
-                     "Only return the corrected paragraph, not the original text or this prompt."
-                     "Here's the text:\n\n" text))))))
-
 ;; S Up, S Down to switch windows
 ;; Commented since it takes over S <left> S <right> in calendar picker in org
 ;; (windmove-default-keybindings)
